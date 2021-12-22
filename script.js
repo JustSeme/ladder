c468cea456577339b86b51faab4dafc8b248edb0
JustSemen <81112954+JustSemen@users.noreply.github.com>
2021-12-22 13:28:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const steps = document.querySelectorAll('.steps')
+
+let ladder = {
+    step: 0,
+    showStep() {
+        alert(this.step + 1)
+        steps[this.step].style.background = 'green'
+    },
+    up() {
+        if(this.step <= 5) this.step++
+        if(this.step === 5) alert('Выше только бог, бро')
+        steps[this.step].style.background = 'green'
+        steps[this.step - 1].style.background = 'red'
+    },
+    down() {
+        if(this.step > 0) this.step--
+        steps[this.step].style.background = 'green'
+        steps[this.step + 1].style.background = 'red'
+        if(this.step === 0) alert('Ниже некуда, бро')
+    }
+}
