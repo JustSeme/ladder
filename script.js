5d20026a9332007b2632201b78243e5786c332af
JustSemen <semyn03@mail.ru>
2022-10-08 22:15:25 +0600

    fix up-down

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ let ladder = {
         steps[this.step].style.background = 'green'
     },
     up() {
-        if(this.step <= 5) this.step++
-        if(this.step === 5) alert('Выше только бог, бро')
+        if (this.step <= 5) this.step++
+        if (this.step === 5) alert('Ниже некуда, бро')
         steps[this.step].style.background = 'green'
         steps[this.step - 1].style.background = 'red'
     },
     down() {
-        if(this.step > 0) this.step--
+        if (this.step > 0) this.step--
         steps[this.step].style.background = 'green'
         steps[this.step + 1].style.background = 'red'
-        if(this.step === 0) alert('Ниже некуда, бро')
+        if (this.step === 0) alert('Выше только бог, бро')
     }
 }
